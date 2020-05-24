5ac6f40312a01211fb6a851aa99a79a49cc17b4c
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2020-05-24 16:53:08 +0900

    Q7 addedC

---
+++ 07_anagrams/index.js
@@ -0,0 +1,21 @@
+// --- 問題
+// 引数で与えられた二つの文字列がアナグラムかどうかを返すメソッドを完成させよ
+
+// アナグラムとは
+// 言葉遊びの一つで、単語または文の中の文字をいくつか入れ替えることによって、全く別の意味にさせる遊び
+// https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%8A%E3%82%B0%E3%83%A9%E3%83%A0
+
+// ・スペース、句読点は考慮しない
+// ・大文字・小文字は区別しない
+
+// --- 例
+// anagrams('rail safety', 'fairy tales') --> true
+// anagrams('RAIL! SAFETY!', 'fairy tales') --> true
+// anagrams('Hi there', 'Bye there') --> false
+
+function anagrams(stringA, stringB) {
+  //　ここに書く
+}
+
+module.exports = anagrams;
+  
---
+++ 07_anagrams/test.js
@@ -0,0 +1,27 @@
+const anagrams = require('./index.js');
+
+test('anagrams関数が定義されている', () => {
+  expect(typeof anagrams).toEqual('function');
+});
+
+test('"hello"は"llohe"のアナグラムである', () => {
+  expect(anagrams('hello', 'llohe')).toBeTruthy();
+});
+
+test('"Whoa! Hi!"は"Hi! Whoa!のアナグラムである"', () => {
+  expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
+});
+
+test('"One One"は"Two two two"のアナグラムではない', () => {
+  expect(anagrams('One One', 'Two two two')).toBeFalsy();
+});
+
+test('"One one"は"One one c"のアナグラムではない', () => {
+  expect(anagrams('One one', 'One one c')).toBeFalsy();
+});
+
+test('"A tree, a life, a bench"は"A tree, a fence, a yard"のアナグラムではない', () => {
+  expect(
+    anagrams('A tree, a life, a bench', 'A tree, a fence, a yard')
+  ).toBeFalsy();
+});
