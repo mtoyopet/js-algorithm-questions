a5abe9f895ce88002065bca6ec8145df4b72d74b
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2020-05-31 17:19:06 +0900

    pyramid added

---
+++ level_3/10_pyramid/test.js
@@ -0,0 +1,38 @@
+
+const pyramid = require('./index');
+
+beforeEach(() => {
+  jest.spyOn(console, 'log');
+});
+
+afterEach(() => {
+  console.log.mockRestore();
+});
+
+test('pyramid関数が定義されていること', () => {
+  expect(typeof pyramid).toEqual('function');
+});
+
+test('n = 2の時ピラミッドをconsole.logする', () => {
+  pyramid(2);
+  expect(console.log.mock.calls[0][0]).toEqual(' # ');
+  expect(console.log.mock.calls[1][0]).toEqual('###');
+  expect(console.log.mock.calls.length).toEqual(2);
+});
+
+test('n = 3の時ピラミッドをconsole.logする', () => {
+  pyramid(3);
+  expect(console.log.mock.calls[0][0]).toEqual('  #  ');
+  expect(console.log.mock.calls[1][0]).toEqual(' ### ');
+  expect(console.log.mock.calls[2][0]).toEqual('#####');
+  expect(console.log.mock.calls.length).toEqual(3);
+});
+
+test('n = 4の時ピラミッドをconsole.logする', () => {
+  pyramid(4);
+  expect(console.log.mock.calls[0][0]).toEqual('   #   ');
+  expect(console.log.mock.calls[1][0]).toEqual('  ###  ');
+  expect(console.log.mock.calls[2][0]).toEqual(' ##### ');
+  expect(console.log.mock.calls[3][0]).toEqual('#######');
+  expect(console.log.mock.calls.length).toEqual(4);
+});
